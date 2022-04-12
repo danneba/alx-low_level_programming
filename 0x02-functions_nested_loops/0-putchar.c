42048867a173b4e35f4d460ad480786753e60600
danneba <80915997+danneba@users.noreply.github.com>
2022-04-12 22:01:05 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /
  * main - entry block
  * @void: no argument
@@ -6,13 +7,13 @@
  /
 int main(void)
 {
-  char dani[] = "_putchar";
+  char ermias[] = "_putchar";
 
   int c;
 
   for (c = 0; c < 8; c++)
   {
-    _putchar(dani[c]);
+    _putchar(ermias[c]);
   }
   _putchar('\n');
   return (0);
